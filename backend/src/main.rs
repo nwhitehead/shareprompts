1aa7df89af0a9792f948021cd84b00448375d7c5
Nathan Whitehead <nwhitehe@gmail.com>
2023-04-02 08:12:49 -0600

    Got logger working.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -8,6 +8,7 @@ use diesel::{prelude::*, r2d2};
 use diesel::pg::PgConnection;
 use std::env;
 use dotenvy::dotenv;
+use env_logger::init;
 
 /// Short-hand for the database pool type to use throughout the app.
 type DbPool = r2d2::Pool<r2d2::ConnectionManager<PgConnection>>;
@@ -15,6 +16,11 @@ type DbPool = r2d2::Pool<r2d2::ConnectionManager<PgConnection>>;
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!\n")
+}
+
+#[get("/post/{id}")]
+async fn get_post(id: web::Path<(i32,)>) -> impl Responder {
+    HttpResponse::Ok().body(format!("Getting post id={}\n", id.into_inner().0))
 }
 
 #[post("/echo")]
@@ -31,10 +37,12 @@ async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
     // initialize DB pool outside of `HttpServer::new` so that it is shared across all workers
     let pool = initialize_db_pool();
-    HttpServer::new(|| {
+    HttpServer::new(move || {
         App::new()
+            .app_data(web::Data::new(pool.clone()))
             .service(hello)
             .service(echo)
+            .service(get_post)
             .route("/hey", web::get().to(manual_hello))
     })
     .bind("0.0.0.0:9090")?
@@ -47,9 +55,10 @@ async fn main() -> std::io::Result<()> {
 /// See more: <https://docs.rs/diesel/latest/diesel/r2d2/index.html>.
 fn initialize_db_pool() -> DbPool {
     dotenv().ok();
+    env_logger::init();
     let conn_spec = std::env::var("DATABASE_URL").expect("DATABASE_URL should be set");
     let manager = r2d2::ConnectionManager::<PgConnection>::new(conn_spec);
     r2d2::Pool::builder()
         .build(manager)
-        .expect("database URL should be valid path to Postgres database")
+        .expect("database URL should be valid path to Postgres database with username and password")
 }
