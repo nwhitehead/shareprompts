dc425e6507fe394209951770f29c14a69d03a110
Nathan Whitehead <nwhitehe@gmail.com>
2023-04-10 17:06:48 -0600

    Make route work

--- site/src/App.vue
+++ site/src/App.vue
@@ -11,7 +11,7 @@
 <template>
     <p><router-link to="/about">Go to About</router-link></p>
     
-    <p><router-link to="/conversation/1234">Test convo</router-link></p>
+    <p><router-link to="/conversation/c535280a53db4947bb85df001359c92d">Test convo</router-link></p>
 
     <router-view></router-view>
 </template>
--- site/src/views/Conversation.vue
+++ site/src/views/Conversation.vue
@@ -1,5 +1,29 @@
+<script setup>
+
+import { onMounted } from 'vue';
+import { useRouter, useRoute } from 'vue-router';
+
+async function getData(id) {
+    console.log('Getting data for conversation', id);
+    const addr = `https://shareconversation.com/api/conversation/${id}`;
+    console.log(`addr=${addr}`);
+    const response = await fetch(addr);
+    const jsondata = await response.json();
+    console.log('Conversation data is', jsondata);
+    return jsondata;
+}
+
+const route = useRoute();
+
+onMounted(async () => {
+    const id = route.params.id;
+    const jsondata = await getData(id);
+});
+
+</script>
+
 <template>
     <h1>A Conversation</h1>
 
-    <p>Here is the conversation you requested: {{ $route.params.id || "none" }}</p>
+    <p>Here is the conversation you requested: {{ $route.params.id }}</p>
 </template>
