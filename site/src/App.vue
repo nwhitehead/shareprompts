4c7bf8ae163def3c00bff3ba9f6c68b742ff6657
Nathan Whitehead <nwhitehe@gmail.com>
2023-04-10 08:43:34 -0600

    Make main site a vue app. Look at url to get id.

---
+++ site/src/App.vue
@@ -0,0 +1,15 @@
+
+<script setup>
+
+const url_params = new URLSearchParams(window.location.search);
+const id = url_params.get('id');
+
+console.log('App.vue setup', url_params);
+
+</script>
+
+<template>
+    <h1>A Conversation</h1>
+
+    <p>Here is the conversation you requested: {{ id || "none" }}</p>
+</template>
