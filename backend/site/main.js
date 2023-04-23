1befbdb18fe99a05ae3d23278ed2910f1800717e
Nathan Whitehead <nwhitehe@gmail.com>
2023-04-23 13:40:24 -0600

    Make code in output look nicer.

--- backend/site/main.js
+++ backend/site/main.js
@@ -6,12 +6,19 @@ export function handleClick() {
 }
 
 window.addEventListener("DOMContentLoaded", (event) => {
-    console.log("DOM fully loaded and parsed");
+    // Get everything in .markdown class, interpret as markdown and update DOM
     const markdown = document.querySelectorAll('.markdown');
     for (let i = 0; i < markdown.length; i++) {
         const node = markdown[i];
         const src = node.innerHTML;
         node.innerHTML = marked.parse(src);
     }
+    // Look for pre code sections, highlight
     hljs.highlightAll();
+    // Update pre code sections with detected language text
+    var blocks = document.querySelectorAll('pre code.hljs');
+    Array.prototype.forEach.call(blocks, function(block) {
+        var language = block.result.language;
+        block.insertAdjacentHTML("beforebegin",`<div class="flex items-center text-stone-200 bg-stone-800 px-4 py-2 text-xs justify-between rounded-t-md"><span>${language}</span></div>`)
+    });
 });
