64a9e4aa60332f8bc0c59923ecab141af7d05aa9
Nathan Whitehead <nwhitehe@gmail.com>
2023-05-01 13:42:54 -0600

    Updated format for markdown download.

--- site/src/App.vue
+++ site/src/App.vue
@@ -68,7 +68,7 @@ function link(id) {
 }
 
 function openai_link(id) {
-    return `https://chat.openai.com/${id}`;
+    return `https://chat.openai.com${id}`;
 }
 
 /// Generic function to convert JS array into CSV and prompt user to save with filename
@@ -77,7 +77,7 @@ function openai_link(id) {
 // comments: [[string]]
 // rows and comments are both in same format
 // comments will be prefixed by "# " and ignored by parser.
-function exportCsv(filename, rows, comments) {
+function exportCsv(filename, rows) {
     function process(row) {
         let res = '';
         for (let j = 0; j < row.length; j++) {
@@ -95,11 +95,6 @@ function exportCsv(filename, rows, comments) {
     }
 
     let csvFile = '';
-    if (comments !== undefined) {
-        for (let i = 0; i < comments.length; i++) {
-            csvFile += '# ' + process(comments[i]);
-        }
-    }
     for (let i = 0; i < rows.length; i++) {
         csvFile += process(rows[i]);
     }
@@ -129,22 +124,22 @@ async function downloadMarkdown(id) {
     };
     const response = await fetch(addr, options);
     const data = await response.json();
-    let comments = [];
-    comments.push(['link', link(data.id)]);
-    const date = data.metadata.creationdate.secs_since_epoch + data.metadata.creationdate.nanos_since_epoch * 1e-9;
-    comments.push(['date', date]);
-    comments.push(['title', data.metadata.title]);
-    comments.push(['avatar', data.contents.avatar]);
-    comments.push(['model', data.metadata.model]);
-    comments.push(['openai_link', openai_link(data.metadata.openaiid)]);
-    comments.push(['length', data.metadata.length]);
     let rows = [];
+    rows.push(['link', link(data.id)]);
+    const date = new Date(data.metadata.creationdate.secs_since_epoch * 1000 + data.metadata.creationdate.nanos_since_epoch * 1e-6).toUTCString();
+    rows.push(['date', date]);
+    rows.push(['title', data.metadata.title]);
+    rows.push(['avatar', data.contents.avatar]);
+    rows.push(['model', data.metadata.model]);
+    rows.push(['openai_link', openai_link(data.metadata.openaiid)]);
+    rows.push(['length', data.metadata.length]);
+    rows.push([]);
     rows.push(['who', 'what']);
     for (let i = 0; i < data.contents.dialog.length; i++) {
         const dialog_row = data.contents.dialog[i];
         rows.push([dialog_row.who, dialog_row.what]);
     }
-    exportCsv(`conversation-${data.id}.csv`, rows, comments);
+    exportCsv(`conversation-${data.id}.csv`, rows);
 }
 
 async function deleteAction(id) {
@@ -296,12 +291,12 @@ span.note {
                         {{ dateRepresentation(item.metadata.creationdate) }}
                     </td>
                     <td class="border-b p-4 dark:border-dark-5">
-                        <button class="btn-blue" @click="downloadMarkdown(item.id)">
+                        <button class="btn-blue" @click="downloadMarkdown(item.id)" title="Download conversation as CSV">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
                             </svg>
                         </button>
-                        <button class="btn-red" @click="deleteAction(item.id)">
+                        <button class="btn-red" @click="deleteAction(item.id)" title="Delete conversation">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                             </svg>
