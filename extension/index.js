103dc5d665347a2bb2cb7d090b76d6779ebf372d
Nathan Whitehead <nwhitehe@gmail.com>
2023-05-01 16:13:33 -0600

    Better positioning of share button in extension.

--- extension/index.js
+++ extension/index.js
@@ -82,21 +82,21 @@ async function handleClick(button) {
 }
 
 function addButton() {
-    const button = document.createElement("button");  
+    const button = document.createElement("button");
     button.id = "share";
     button.classList.add("btn", "flex", "gap-2", "justify-center", "btn-neutral");
     // svg icon here is something I drew in inkscape and optimized online
     button.innerHTML = `<?xml version="1.0" encoding="UTF-8"?>
 <svg width="5mm" height="5mm" version="1.1" viewBox="0 0 9.0162 8.8533" xmlns="http://www.w3.org/2000/svg">
 <g transform="translate(-4.3461 -4.7617)">
-<path d="m9.1966 6.8633c0.0058-0.70053 0.01156-1.4011 0.01734-2.1016l2.0717 1.6875c1.1394 0.92814 2.0739 1.694 2.0765 1.702 0.0042 0.01269-4.1045 3.3842-4.1422 3.3991-0.02249-0.73726-0.01471-1.3-0.01471-2.0956l-0.03901 8e-3c-2.3433 0.14879-3.4247 1.1338-4.6528 4.1523-0.87542-5.5541 1.8295-6.4247 4.6831-6.7516z" fill="#d9d9e3" stroke-width=".017338"/>
+<path d="m9.1966 6.8633c0.0058-0.70053 0.01156-1.4011 0.01734-2.1016l2.0717 1.6875c1.1394 0.92814 2.0739 1.694 2.0765 1.702 0.0042 0.01269-4.1045 3.3842-4.1422 3.3991-0.02249-0.73726-0.01471-1.3-0.01471-2.0956l-0.03901 8e-3c-2.3433 0.14879-3.4247 1.1338-4.6528 4.1523-0.87542-5.5541 1.8295-6.4247 4.6831-6.7516z" fill="rgb(64,65,79)" stroke-width=".017338"/>
 </g>
 </svg> Share`;
     button.addEventListener("click", async function () {
         handleClick(button);
     });
-    const buttonsWrapper = document.querySelector("main form div");
-    buttonsWrapper.appendChild(button);
+    const buttonsWrapper = document.querySelector("main form div button.relative");
+    buttonsWrapper.insertAdjacentElement("afterend", button);
 }
 
 function ensureButton() {
