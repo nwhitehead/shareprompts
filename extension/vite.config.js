8bd4a66b6090ae30b2f22c792fda132bfce7dfa4
Nathan Whitehead <nwhitehe@gmail.com>
2023-05-10 14:48:51 -0600

    Convert to vite-plugin-webextension from crxjs.

--- extension/vite.config.js
+++ extension/vite.config.js
@@ -1,14 +1,13 @@
-import { resolve } from 'path';
 import { defineConfig } from 'vite';
-import { crx } from '@crxjs/vite-plugin'
 import manifest from './manifest.json' assert { type: 'json' } // Node >=17
 import vue from '@vitejs/plugin-vue';
+import webExtension from "@samrum/vite-plugin-web-extension";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: "",
     plugins: [
         vue(),
-        crx({ manifest })
+        webExtension({ manifest }),
     ],
 })
