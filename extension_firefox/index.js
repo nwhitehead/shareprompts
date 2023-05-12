a9d4b12bed65313e8de041c8bd3d95c4843f20a0
Nathan Whitehead <nwhitehe@gmail.com>
2023-05-12 08:56:24 -0600

    Fix firefox warning with innerHTML for share button (still have it for vue).
    Add clipboard write permission, update algo to wait for clipboard update from fixed string.

--- extension_firefox/index.js
+++ extension_firefox/index.js
@@ -2,12 +2,29 @@ import ExtPay from "extpay";
 const client_id = 'share-conversations';
 let extpay = ExtPay(client_id);
 
-const shareButtonContent = `<?xml version="1.0" encoding="UTF-8"?>
-<svg width="5mm" height="5mm" version="1.1" viewBox="0 0 9.0162 8.8533" xmlns="http://www.w3.org/2000/svg">
-<g transform="translate(-4.3461 -4.7617)">
-<path d="m9.1966 6.8633c0.0058-0.70053 0.01156-1.4011 0.01734-2.1016l2.0717 1.6875c1.1394 0.92814 2.0739 1.694 2.0765 1.702 0.0042 0.01269-4.1045 3.3842-4.1422 3.3991-0.02249-0.73726-0.01471-1.3-0.01471-2.0956l-0.03901 8e-3c-2.3433 0.14879-3.4247 1.1338-4.6528 4.1523-0.87542-5.5541 1.8295-6.4247 4.6831-6.7516z" fill="rgb(64,65,79)" stroke-width=".017338"/>
-</g>
-</svg> Share`;
+// Delay in ms between clicking on button and copying clipboard
+const CLICK_COPY_DELAY = 5;
+// Maximum number of times to poll clipboard before giving up
+const MAX_LOOPS = 200;
+// Special initial token to represent no change in clipboard (so we know when it changes)
+const SPECIAL_EMPTY = "191765971691113173561678--EMPTY--18673571916517656727";
+
+function addShareButtonContent(node) {
+    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    svg.setAttribute('width', '5mm');
+    svg.setAttribute('height', '5mm');
+    svg.setAttribute('version', '1.1');
+    svg.setAttribute('viewBox', '0 0 9.0162 8.8533');
+    const g = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+    g.setAttribute('transform', 'translate(-4.3461 -4.7617)');
+    const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    path.setAttribute('d', 'm9.1966 6.8633c0.0058-0.70053 0.01156-1.4011 0.01734-2.1016l2.0717 1.6875c1.1394 0.92814 2.0739 1.694 2.0765 1.702 0.0042 0.01269-4.1045 3.3842-4.1422 3.3991-0.02249-0.73726-0.01471-1.3-0.01471-2.0956l-0.03901 8e-3c-2.3433 0.14879-3.4247 1.1338-4.6528 4.1523-0.87542-5.5541 1.8295-6.4247 4.6831-6.7516z');
+    path.setAttribute('fill', 'rgb(64,65,79)');
+    path.setAttribute('stroke-width', '.017338');
+    g.appendChild(path);
+    svg.appendChild(g);
+    node.replaceChildren(...[svg, " Share"]);
+}
 
 async function handleClick(button) {
 
@@ -53,10 +70,20 @@ async function handleClick(button) {
                 });
             }
         } else if (markdown) {
+            await navigator.clipboard.writeText(SPECIAL_EMPTY);
             const buttons = node.querySelectorAll('button.flex.ml-auto');
             const button = buttons[buttons.length - 1];
             button.click();
-            const txt = await navigator.clipboard.readText();
+            let txt = SPECIAL_EMPTY;
+            let loops = 0;
+            while (txt === SPECIAL_EMPTY && loops < MAX_LOOPS) {
+                await new Promise(r => setTimeout(r, CLICK_COPY_DELAY));
+                txt = await navigator.clipboard.readText();
+            }
+            if (loops === MAX_LOOPS) {
+                console.log('Exceeded maximum number of loops to retrieve ChatGPT text');
+                txt = '';
+            }
             conversationData.dialog.push({
                 who: "gpt",
                 what: txt,
@@ -101,7 +128,7 @@ async function handleClick(button) {
     } catch(error) {
         console.log('There was an error during sharing', error);
     }
-    button.innerHTML = shareButtonContent;
+    addShareButtonContent(button);
     button.style.cursor = "pointer";
     button.disabled = false;
 }
@@ -113,7 +140,7 @@ function addButton() {
         button.id = "share";
         button.classList.add("btn", "flex", "gap-2", "justify-center", "btn-neutral");
         // svg icon here is something I drew in inkscape and optimized online
-        button.innerHTML = shareButtonContent;
+        addShareButtonContent(button);
         button.addEventListener("click", async function () {
             handleClick(button);
         });
